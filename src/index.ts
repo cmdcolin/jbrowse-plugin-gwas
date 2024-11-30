a614beb53ce737376bd10d300fa9fbd9408b6345
Colin <colin.diesh@gmail.com>
2024-11-30 17:43:49 -0500

    Wow

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import Plugin from '@jbrowse/core/Plugin'
 
 import LinearManhattanDisplayF from './LinearManhattanDisplay'
 import LinearManhattanRendererF from './LinearManhattanRenderer'
+import { version } from './version'
 
 import type PluginManager from '@jbrowse/core/PluginManager'
 
 export default class GWASPlugin extends Plugin {
   name = 'GWASPlugin'
+  version = version
 
   install(pluginManager: PluginManager) {
     LinearManhattanDisplayF(pluginManager)
