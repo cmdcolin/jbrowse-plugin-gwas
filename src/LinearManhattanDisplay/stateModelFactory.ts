49e795b26bae4ae783ed787f141600657a3f4ddd
Colin <colin.diesh@gmail.com>
2025-02-05 13:41:51 -0500

    Better types

--- src/LinearManhattanDisplay/stateModelFactory.ts
+++ src/LinearManhattanDisplay/stateModelFactory.ts
@@ -1,3 +1,7 @@
+import { lazy } from 'react'
+
+import { ConfigurationReference, getConf } from '@jbrowse/core/configuration'
+import SerializableFilterChain from '@jbrowse/core/pluggableElementTypes/renderers/util/serializableFilterChain'
 import {
   getContainingTrack,
   getContainingView,
@@ -5,33 +9,30 @@ import {
   isSelectionContainer,
   isSessionModelWithWidgets,
 } from '@jbrowse/core/util'
+import { cast, types } from 'mobx-state-tree'
 
 import TooltipComponent from './TooltipComponent'
 
 import type PluginManager from '@jbrowse/core/PluginManager'
+import type { AnyConfigurationSchemaType } from '@jbrowse/core/configuration'
+import type { MenuItem } from '@jbrowse/core/ui'
 import type { Feature } from '@jbrowse/core/util'
 import type WigglePlugin from '@jbrowse/plugin-wiggle'
 
-import type { MenuItem } from '@jbrowse/core/ui'
-import { lazy } from 'react'
+// lazies
 const AddFiltersDialog = lazy(() => import('./AddFiltersDialog'))
-import { cast, getEnv, types } from 'mobx-state-tree'
-
-import { ConfigurationReference, getConf } from '@jbrowse/core/configuration'
-import SerializableFilterChain from '@jbrowse/core/pluggableElementTypes/renderers/util/serializableFilterChain'
 
 export function stateModelFactory(
   pluginManager: PluginManager,
-  configSchema: any,
+  configSchema: AnyConfigurationSchemaType,
 ) {
-  const { types } = pluginManager.lib['mobx-state-tree']
   const WigglePlugin = pluginManager.getPlugin('WigglePlugin') as WigglePlugin
   const { linearWiggleDisplayModelFactory } = WigglePlugin.exports
-  return types.compose(
-    'LinearManhattanDisplay',
-    linearWiggleDisplayModelFactory(pluginManager, configSchema),
-    types
-      .model({
+  return types
+    .compose(
+      'LinearManhattanDisplay',
+      linearWiggleDisplayModelFactory(pluginManager, configSchema),
+      types.model({
         type: types.literal('LinearManhattanDisplay'),
         /**
          * #property
@@ -41,36 +42,72 @@ export function stateModelFactory(
          * #property
          */
         jexlFilters: types.maybe(types.array(types.string)),
-      })
-      .views(self => {
-        const {
-          trackMenuItems: superTrackMenuItems,
-          renderProps: superRenderProps,
-        } = self
-        return {
-        get TooltipComponent() {
-          return TooltipComponent
-        },
-        get rendererTypeName() {
-          return 'LinearManhattanRenderer'
-        },
-        get needsScalebar() {
-          return true
-        },
-        get regionTooLarge() {
-          return false
-        },
-        /**
-         * #getter
-         */
-        get activeFilters() {
-          // config jexlFilters are deferred evaluated so they are prepended with
-          // jexl at runtime rather than being stored with jexl in the config
-          return (
-            self.jexlFilters ??
-            getConf(self, 'jexlFilters').map((r: string) => `jexl:${r}`)
+      }),
+    )
+    .views(self => ({
+      get TooltipComponent() {
+        return TooltipComponent
+      },
+      get rendererTypeName() {
+        return 'LinearManhattanRenderer'
+      },
+      get needsScalebar() {
+        return true
+      },
+      get regionTooLarge() {
+        return false
+      },
+      /**
+       * #getter
+       */
+      get activeFilters() {
+        // config jexlFilters are deferred evaluated so they are prepended with
+        // jexl at runtime rather than being stored with jexl in the config
+        return (
+          self.jexlFilters ??
+          getConf(self, 'jexlFilters').map((r: string) => `jexl:${r}`)
+        )
+      },
+    }))
+    .actions(self => ({
+      /**
+       * #action
+       * this overrides the BaseLinearDisplayModel to avoid popping up a
+       * feature detail display, but still sets the feature selection on the
+       * model so listeners can detect a click
+       */
+      selectFeature(feature: Feature) {
+        const session = getSession(self)
+        if (isSessionModelWithWidgets(session)) {
+          const featureWidget = session.addWidget(
+            'BaseFeatureWidget',
+            'baseFeature',
+            {
+              view: getContainingView(self),
+              track: getContainingTrack(self),
+              featureData: feature.toJSON(),
+            },
           )
-        },
+
+          session.showWidget(featureWidget)
+        }
+        if (isSelectionContainer(session)) {
+          session.setSelection(feature)
+        }
+      },
+      /**
+       * #action
+       */
+      setJexlFilters(f?: string[]) {
+        self.jexlFilters = cast(f)
+      },
+    }))
+    .views(self => {
+      const {
+        trackMenuItems: superTrackMenuItems,
+        renderProps: superRenderProps,
+      } = self
+      return {
         /**
          * #method
          */
@@ -95,47 +132,17 @@ export function stateModelFactory(
               onClick: () => {
                 getSession(self).queueDialog(handleClose => [
                   AddFiltersDialog,
-                  { model: self, handleClose },
+                  {
+                    model: self,
+                    handleClose,
+                  },
                 ])
               },
             },
           ]
         },
-      }})
-      .actions(self => ({
-        /**
-         * #action
-         * this overrides the BaseLinearDisplayModel to avoid popping up a
-         * feature detail display, but still sets the feature selection on the
-         * model so listeners can detect a click
-         */
-        selectFeature(feature: Feature) {
-          const session = getSession(self)
-          if (isSessionModelWithWidgets(session)) {
-            const featureWidget = session.addWidget(
-              'BaseFeatureWidget',
-              'baseFeature',
-              {
-                view: getContainingView(self),
-                track: getContainingTrack(self),
-                featureData: feature.toJSON(),
-              },
-            )
-
-            session.showWidget(featureWidget)
-          }
-          if (isSelectionContainer(session)) {
-            session.setSelection(feature)
-          }
-        },
-        /**
-         * #action
-         */
-        setJexlFilters(f?: string[]) {
-          self.jexlFilters = cast(f)
-        },
-      })),
-  )
+      }
+    })
 }
 
 export type LinearManhattanDisplayModel = ReturnType<typeof stateModelFactory>
