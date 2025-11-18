be960f12e04ed76dff09f315abbbb567c2f388bd
Colin <colin.diesh@gmail.com>
2025-11-18 10:37:27 -0500

    Finish

--- src/LinearManhattanRenderer/LinearManhattanRendering.tsx
+++ src/LinearManhattanRenderer/LinearManhattanRendering.tsx
@@ -3,7 +3,7 @@ import React, { useMemo, useRef } from 'react'
 import { PrerenderedCanvas } from '@jbrowse/core/ui'
 import { SimpleFeature } from '@jbrowse/core/util'
 import { observer } from 'mobx-react'
-import RBush from 'rbush'
+import Flatbush from 'flatbush'
 
 import type { Feature, SimpleFeatureSerialized } from '@jbrowse/core/util'
 import type { Region } from '@jbrowse/core/util/types'
@@ -22,13 +22,13 @@ const LinearManhattanRendering = observer(function (props: {
   onFeatureClick?: (event: React.MouseEvent, arg?: string) => void
 }) {
   const { height, onMouseLeave, onMouseMove, onFeatureClick, clickMap } = props
-  const clickMap2 = useMemo(() => {
-    return new RBush<{ feature: SimpleFeatureSerialized }>().fromJSON(clickMap)
+  const clickMapData = useMemo(() => {
+    const index = Flatbush.from(clickMap.index)
+    return { index, items: clickMap.items }
   }, [clickMap])
   const ref = useRef<HTMLDivElement>(null)
 
   function getFeatureUnderMouse(eventClientX: number, eventClientY: number) {
-    // calculates feature under mouse
     let offsetX = 0
     let offsetY = 0
     if (ref.current) {
@@ -36,13 +36,16 @@ const LinearManhattanRendering = observer(function (props: {
       offsetX = eventClientX - r.left
       offsetY = eventClientY - r.top
     }
-    const ret = clickMap2.search({
-      minX: offsetX,
-      minY: offsetY,
-      maxX: offsetX + 3,
-      maxY: offsetY + 3,
-    })
-    return ret[0] ? new SimpleFeature(ret[0].feature) : undefined
+    const indices = clickMapData.index.search(
+      offsetX,
+      offsetY,
+      offsetX + 3,
+      offsetY + 3,
+    )
+    const firstIndex = indices[0]
+    return firstIndex !== undefined
+      ? new SimpleFeature(clickMapData.items[firstIndex]?.feature)
+      : undefined
   }
   return (
     <div
