df4ca5d428fa09411a0ee328d6e4a824c8fc213a
Colin <colin.diesh@gmail.com>
2025-11-30 19:07:52 -0500

    Updates

--- src/LinearManhattanRenderer/LinearManhattanRenderer.ts
+++ src/LinearManhattanRenderer/LinearManhattanRenderer.ts
@@ -14,8 +14,6 @@ import Flatbush from 'flatbush'
 
 import { checkStopToken } from './util'
 
-import type PluginManager from '@jbrowse/core/PluginManager'
-
 const TWO_PI = Math.PI * 2
 const YSCALEBAR_LABEL_OFFSET = 5
 const POINT_RADIUS = 2
@@ -39,184 +37,184 @@ interface ManhattanRenderProps {
   highResolutionScaling?: number
 }
 
-export default function rendererFactory(_pluginManager: PluginManager) {
-  return class ManhattanPlotRenderer extends FeatureRendererType {
-    supportsSVG = true
+export default class ManhattanPlotRenderer extends FeatureRendererType {
+  supportsSVG = true
 
-    async render(renderProps: ManhattanRenderProps) {
-      const features = await this.getFeatures(renderProps)
-      const { height, regions, bpPerPx, statusCallback = () => {} } = renderProps
+  async render(renderProps: ManhattanRenderProps) {
+    const features = await this.getFeatures(renderProps)
+    const { height, regions, bpPerPx, statusCallback = () => {} } = renderProps
 
-      const region = regions[0]!
-      const width = (region.end - region.start) / bpPerPx
+    const region = regions[0]!
+    const width = (region.end - region.start) / bpPerPx
 
-      const { reducedFeatures, ...rest } = await updateStatus(
-        'Rendering plot',
-        statusCallback,
-        () =>
-          renderToAbstractCanvas(width, height, renderProps, ctx =>
-            this.draw(ctx, {
-              ...renderProps,
-              features,
-            }),
-          ),
-      )
+    const { reducedFeatures, ...rest } = await updateStatus(
+      'Rendering plot',
+      statusCallback,
+      () =>
+        renderToAbstractCanvas(width, height, renderProps, ctx =>
+          this.draw(ctx, {
+            ...renderProps,
+            features,
+          }),
+        ),
+    )
 
-      const results = await super.render({
-        ...renderProps,
-        ...rest,
-        features,
-        height,
-        width,
-      })
+    const results = await super.render({
+      ...renderProps,
+      ...rest,
+      features,
+      height,
+      width,
+    })
 
-      return {
-        ...results,
-        ...rest,
-        features: reducedFeatures
-          ? new Map<string, Feature>(reducedFeatures.map(r => [r.id(), r]))
-          : results.features,
-        height,
-        width,
-        containsNoTransferables: true,
+    return {
+      ...results,
+      ...rest,
+      features: reducedFeatures
+        ? new Map<string, Feature>(reducedFeatures.map(r => [r.id(), r]))
+        : results.features,
+      height,
+      width,
+      containsNoTransferables: true,
+    }
+  }
+
+  draw(
+    ctx: CanvasRenderingContext2D,
+    props: ManhattanRenderProps & { features: Map<string, Feature> },
+  ) {
+    const {
+      features,
+      regions,
+      bpPerPx,
+      config,
+      scaleOpts,
+      height: unadjustedHeight,
+      displayCrossHatches,
+      ticks: { values },
+      stopToken,
+    } = props
+    const region = regions[0]!
+    const height = unadjustedHeight - YSCALEBAR_LABEL_OFFSET * 2
+    const width = (region.end - region.start) / bpPerPx
+    const pxPerBp = 1 / bpPerPx
+    const regionStart = region.start
+
+    const scale = getScale({
+      ...scaleOpts,
+      range: [0, height],
+    })
+    const yOffset = height + YSCALEBAR_LABEL_OFFSET
+    const toY = (n: number) => yOffset - scale(n)
+
+    const { isCallback } = config.color
+    const colorValue = config.color.value as string
+    const canBatch =
+      !isCallback &&
+      !colorValue.includes('rgba') &&
+      !colorValue.includes('hsla')
+    const items: {
+      minX: number
+      minY: number
+      maxX: number
+      maxY: number
+      feature: any
+    }[] = []
+
+    let lastRenderedBlobX = 0
+    let lastRenderedBlobY = 0
+    let start = performance.now()
+    let i = 0
+
+    if (canBatch) {
+      ctx.fillStyle = colorValue
+      ctx.beginPath()
+      for (const feature of features.values()) {
+        if (i++ % 100 === 0 && performance.now() - start > 200) {
+          checkStopToken(stopToken)
+          start = performance.now()
+        }
+        const leftPx = (feature.get('start') - regionStart) * pxPerBp
+        const y = toY(feature.get('score'))
+        if (
+          Math.abs(leftPx - lastRenderedBlobX) > 1 ||
+          Math.abs(y - lastRenderedBlobY) > 1
+        ) {
+          ctx.moveTo(leftPx + POINT_RADIUS, y)
+          ctx.arc(leftPx, y, POINT_RADIUS, 0, TWO_PI)
+          lastRenderedBlobY = y
+          lastRenderedBlobX = leftPx
+          items.push({
+            minX: leftPx - POINT_RADIUS,
+            minY: y - POINT_RADIUS,
+            maxX: leftPx + POINT_RADIUS,
+            maxY: y + POINT_RADIUS,
+            feature: feature.toJSON(),
+          })
+        }
+      }
+      ctx.fill()
+    } else {
+      if (!isCallback) {
+        ctx.fillStyle = colorValue
+      }
+      for (const feature of features.values()) {
+        if (i++ % 100 === 0 && performance.now() - start > 200) {
+          checkStopToken(stopToken)
+          start = performance.now()
+        }
+        const leftPx = (feature.get('start') - regionStart) * pxPerBp
+        const y = toY(feature.get('score'))
+        if (
+          Math.abs(leftPx - lastRenderedBlobX) > 1 ||
+          Math.abs(y - lastRenderedBlobY) > 1
+        ) {
+          if (isCallback) {
+            ctx.fillStyle = readConfObject(config, 'color', { feature })
+          }
+          ctx.beginPath()
+          ctx.arc(leftPx, y, POINT_RADIUS, 0, TWO_PI)
+          ctx.fill()
+          lastRenderedBlobY = y
+          lastRenderedBlobX = leftPx
+          items.push({
+            minX: leftPx - POINT_RADIUS,
+            minY: y - POINT_RADIUS,
+            maxX: leftPx + POINT_RADIUS,
+            maxY: y + POINT_RADIUS,
+            feature: feature.toJSON(),
+          })
+        }
       }
     }
 
-    async draw(
-      ctx: CanvasRenderingContext2D,
-      props: ManhattanRenderProps & { features: Map<string, Feature> },
-    ) {
-      const {
-        features,
-        regions,
-        bpPerPx,
-        config,
-        scaleOpts,
-        height: unadjustedHeight,
-        displayCrossHatches,
-        ticks: { values },
-        stopToken,
-      } = props
-      const region = regions[0]!
-      const height = unadjustedHeight - YSCALEBAR_LABEL_OFFSET * 2
-      const width = (region.end - region.start) / bpPerPx
-      const regionStart = region.start
-
-      const scale = getScale({
-        ...scaleOpts,
-        range: [0, height],
-      })
-      const toY = (n: number) => height - scale(n) + YSCALEBAR_LABEL_OFFSET
-
-      const { isCallback } = config.color
-      const colorValue = config.color.value as string
-      const canBatch =
-        !isCallback && !colorValue.includes('rgba') && !colorValue.includes('hsla')
-      const items: {
-        minX: number
-        minY: number
-        maxX: number
-        maxY: number
-        feature: any
-      }[] = []
-
-      let lastRenderedBlobX = 0
-      let lastRenderedBlobY = 0
-      let start = performance.now()
-      let i = 0
-
-      if (canBatch) {
-        ctx.fillStyle = colorValue
-        ctx.beginPath()
-        for (const feature of features.values()) {
-          if (i++ % 100 === 0 && performance.now() - start > 200) {
-            checkStopToken(stopToken)
-            start = performance.now()
-          }
-          const leftPx = (feature.get('start') - regionStart) / bpPerPx
-          const score = feature.get('score') as number
-          const y = toY(score)
-          if (
-            Math.abs(leftPx - lastRenderedBlobX) > 1 ||
-            Math.abs(y - lastRenderedBlobY) > 1
-          ) {
-            ctx.moveTo(leftPx + POINT_RADIUS, y)
-            ctx.arc(leftPx, y, POINT_RADIUS, 0, TWO_PI)
-            lastRenderedBlobY = y
-            lastRenderedBlobX = leftPx
-            items.push({
-              minX: leftPx - POINT_RADIUS,
-              minY: y - POINT_RADIUS,
-              maxX: leftPx + POINT_RADIUS,
-              maxY: y + POINT_RADIUS,
-              feature: feature.toJSON(),
-            })
-          }
-        }
-        ctx.fill()
-      } else {
-        if (!isCallback) {
-          ctx.fillStyle = colorValue
-        }
-        for (const feature of features.values()) {
-          if (i++ % 100 === 0 && performance.now() - start > 200) {
-            checkStopToken(stopToken)
-            start = performance.now()
-          }
-          const leftPx = (feature.get('start') - regionStart) / bpPerPx
-          const score = feature.get('score') as number
-          const y = toY(score)
-          if (
-            Math.abs(leftPx - lastRenderedBlobX) > 1 ||
-            Math.abs(y - lastRenderedBlobY) > 1
-          ) {
-            if (isCallback) {
-              ctx.fillStyle = readConfObject(config, 'color', { feature })
-            }
-            ctx.beginPath()
-            ctx.arc(leftPx, y, POINT_RADIUS, 0, TWO_PI)
-            ctx.fill()
-            lastRenderedBlobY = y
-            lastRenderedBlobX = leftPx
-            items.push({
-              minX: leftPx - POINT_RADIUS,
-              minY: y - POINT_RADIUS,
-              maxX: leftPx + POINT_RADIUS,
-              maxY: y + POINT_RADIUS,
-              feature: feature.toJSON(),
-            })
-          }
-        }
+    if (displayCrossHatches) {
+      ctx.lineWidth = 1
+      ctx.strokeStyle = 'rgba(200,200,200,0.8)'
+      ctx.beginPath()
+      for (const tick of values) {
+        const y = Math.round(toY(tick))
+        ctx.moveTo(0, y)
+        ctx.lineTo(width, y)
       }
+      ctx.stroke()
+    }
 
-      if (displayCrossHatches) {
-        ctx.lineWidth = 1
-        ctx.strokeStyle = 'rgba(200,200,200,0.8)'
-        ctx.beginPath()
-        for (const tick of values) {
-          const y = Math.round(toY(tick))
-          ctx.moveTo(0, y)
-          ctx.lineTo(width, y)
-        }
-        ctx.stroke()
+    const index = new Flatbush(Math.max(items.length, 1))
+    if (items.length === 0) {
+      index.add(0, 0, 0, 0)
+    } else {
+      for (const item of items) {
+        index.add(item.minX, item.minY, item.maxX, item.maxY)
       }
+    }
+    index.finish()
 
-      const index = new Flatbush(Math.max(items.length, 1))
-      if (items.length === 0) {
-        index.add(0, 0, 0, 0)
-      } else {
-        for (const item of items) {
-          index.add(item.minX, item.minY, item.maxX, item.maxY)
-        }
-      }
-      index.finish()
-
-      return {
-        clickMap: {
-          index: index.data,
-          items,
-        },
-      }
+    return {
+      clickMap: {
+        index: index.data,
+        items,
+      },
     }
   }
 }
--- src/LinearManhattanRenderer/index.ts
+++ src/LinearManhattanRenderer/index.ts
@@ -1,17 +1,17 @@
-import rendererFactory from './LinearManhattanRenderer'
+import ManhattanPlotRenderer from './LinearManhattanRenderer'
 import LinearManhattanRendering from './LinearManhattanRendering'
 import { configSchema } from './configSchema'
 
 import type PluginManager from '@jbrowse/core/PluginManager'
 
 export default function LinearManhattanRendererF(pluginManager: PluginManager) {
-  pluginManager.addRendererType(() => {
-    const LinearManhattanRenderer = rendererFactory(pluginManager)
-    return new LinearManhattanRenderer({
-      name: 'LinearManhattanRenderer',
-      ReactComponent: LinearManhattanRendering,
-      configSchema,
-      pluginManager,
-    })
-  })
+  pluginManager.addRendererType(
+    () =>
+      new ManhattanPlotRenderer({
+        name: 'LinearManhattanRenderer',
+        ReactComponent: LinearManhattanRendering,
+        configSchema,
+        pluginManager,
+      }),
+  )
 }
